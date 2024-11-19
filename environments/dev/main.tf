a2de081668b6222363520b45c753849cdc458c2f
Bryan <huginnhacks@gmail.com>
2024-11-19 00:17:38 -0700

    Added EC2 module and configuration to main.tf

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -5,7 +5,7 @@ provider "aws" {
 
 module "web_server" {
   source        = "../../modules/ec2"
-  ami_id        = "ami-12345678"
+  ami_id        = "ami-012967cc5a8c9f891"
   instance_type = "t2.micro"
   name          = "dev-web-server"
 
---
+++ modules/ec2/main.tf
@@ -0,0 +1,26 @@
+variable "ami_id" {
+  description = "The AMI ID for the EC2 instance"
+  type        = string
+}
+
+variable "instance_type" {
+  description = "The instance type of the EC2 instance"
+  type        = string
+}
+
+variable "name" {
+  description = "The name of the EC2 instance"
+  type        = string
+}
+
+resource "aws_instance" "this" {
+  ami           = var.ami_id            # Use the value of ami_id passed to the module
+  instance_type = var.instance_type     # Use the value of instance_type passed to the module
+  tags = {
+    Name = var.name                      # Use the value of name passed to the module
+  }
+}
+
+output "instance_id" {
+  value = aws_instance.this.id
+}
