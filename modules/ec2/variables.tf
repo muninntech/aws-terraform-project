a2167267a1cc560222292f4896cceed78dd75b77
Bryan <huginnhacks@gmail.com>
2024-11-22 22:55:14 -0700

    Added EC2 module Outputs and Variables files

---
+++ modules/ec2/variables.tf
@@ -0,0 +1,27 @@
+variable "instance_type" {
+  default = "t2.micro"
+  description = "Instance type for EC2"
+}
+
+variable "key_name" {
+  description = "Key pair name for SSH access"
+  default     = ""
+}
+
+variable "security_group_rules" {
+  type = list(object({
+    from_port   = number
+    to_port     = number
+    protocol    = string
+    cidr_blocks = list(string)
+  }))
+  description = "List of security group rules"
+  default     = [
+    {
+      from_port   = 22
+      to_port     = 22
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+  ]
+}
